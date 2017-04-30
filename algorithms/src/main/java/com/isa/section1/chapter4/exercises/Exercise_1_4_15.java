51c0f4062c1b1b50e1fe505587582824ee1e15a3
isaolmez <isaolmez@gmail.com>
2017-05-01 02:21:25 +0300

    added more examples

--- algorithms/src/main/java/com/isa/section1/chapter4/exercises/Exercise_1_4_15.java
+++ algorithms/src/main/java/com/isa/section1/chapter4/exercises/Exercise_1_4_15.java
@@ -8,105 +8,110 @@ import com.sedgewick.stdlib.StdOut;
 import com.sedgewick.stdlib.Stopwatch;
 
 public class Exercise_1_4_15 {
-	public static void main(String[] args) {
-		// TwoSumFaster twoSumFaster = new TwoSumFaster();
-		// System.out.println(twoSumFaster.count(new int[] { 1, 0, -1, 2, -2, 3, 4, 5, -5 }));
-		// twoSumFaster.ratio(100);
-		ThreeSumFaster threeSumFaster = new ThreeSumFaster();
+    public static void main(String[] args) {
+        // TwoSumFaster twoSumFaster = new TwoSumFaster();
+        // System.out.println(twoSumFaster.count(new int[] { 1, 0, -1, 2, -2, 3, 4, 5, -5 }));
+        // twoSumFaster.ratio(100);
+        ThreeSumFaster threeSumFaster = new ThreeSumFaster();
 //		System.out.println(threeSumFaster.count(new int[] { 1, 0, -1, 2, -2 }));
-		//threeSumFaster.ratio(100);
-		
-		System.out.println("1Kints.txt");
-		String fileName = Thread.currentThread().getContextClassLoader().getResource("1Kints.txt").toString();
-		int[] a = In.readInts(fileName);
-		Stopwatch st = new Stopwatch();
-		StdOut.println(threeSumFaster.count(a));
-		System.out.println("Elapsed Time: " + st.elapsedTime());
+        //threeSumFaster.ratio(100);
 
-		System.out.println("2Kints.txt");
-		fileName = Thread.currentThread().getContextClassLoader().getResource("2Kints.txt").toString();
-		a = In.readInts(fileName);
-		st = new Stopwatch();
-		StdOut.println(threeSumFaster.count(a));
-		System.out.println("Elapsed Time: " + st.elapsedTime());
+        System.out.println("1Kints.txt");
+        String fileName = Thread.currentThread().getContextClassLoader().getResource("1Kints.txt").toString();
+        int[] a = In.readInts(fileName);
+        Stopwatch st = new Stopwatch();
+        StdOut.println(threeSumFaster.count(a));
+        System.out.println("Elapsed Time: " + st.elapsedTime());
 
-		System.out.println("4Kints.txt");
-		fileName = Thread.currentThread().getContextClassLoader().getResource("4Kints.txt").toString();
-		a = In.readInts(fileName);
-		st = new Stopwatch();
-		StdOut.println(threeSumFaster.count(a));
-		System.out.println("Elapsed Time: " + st.elapsedTime());
+        System.out.println("2Kints.txt");
+        fileName = Thread.currentThread().getContextClassLoader().getResource("2Kints.txt").toString();
+        a = In.readInts(fileName);
+        st = new Stopwatch();
+        StdOut.println(threeSumFaster.count(a));
+        System.out.println("Elapsed Time: " + st.elapsedTime());
 
-		System.out.println("8Kints.txt");
-		fileName = Thread.currentThread().getContextClassLoader().getResource("8Kints.txt").toString();
-		a = In.readInts(fileName);
-		st = new Stopwatch();
-		StdOut.println(threeSumFaster.count(a));
-		System.out.println("Elapsed Time: " + st.elapsedTime());
-	}
-}
+        System.out.println("4Kints.txt");
+        fileName = Thread.currentThread().getContextClassLoader().getResource("4Kints.txt").toString();
+        a = In.readInts(fileName);
+        st = new Stopwatch();
+        StdOut.println(threeSumFaster.count(a));
+        System.out.println("Elapsed Time: " + st.elapsedTime());
 
-/** Growth rate: N^2 */
-class TwoSumFaster extends DoublingTestTemplate {
-	public int count(int[] arr) {
-		int count = 0;
-		// Sort -> NlogN
-		Arrays.sort(arr);
+        System.out.println("8Kints.txt");
+        fileName = Thread.currentThread().getContextClassLoader().getResource("8Kints.txt").toString();
+        a = In.readInts(fileName);
+        st = new Stopwatch();
+        StdOut.println(threeSumFaster.count(a));
+        System.out.println("Elapsed Time: " + st.elapsedTime());
+    }
 
-		// Linear time
-		int leftIndex = 0;
-		int rightIndex = arr.length - 1;
-		while (leftIndex < rightIndex && arr[leftIndex] < 0 && arr[rightIndex] > 0) {
-			if (-arr[leftIndex] == arr[rightIndex]) {
-				System.out.println("Numbers: " + arr[leftIndex]);
-				count++;
-				leftIndex++;
-				rightIndex--;
-			} else if (-arr[leftIndex] > arr[rightIndex]) {
-				leftIndex++;
-			} else if (-arr[leftIndex] < arr[rightIndex]) {
-				rightIndex--;
-			}
-		}
+    /**
+     * Growth rate: N^2
+     */
+    private static class TwoSumFaster extends DoublingTestTemplate {
+        public int count(int[] arr) {
+            int count = 0;
+            // Sort -> NlogN
+            Arrays.sort(arr);
 
-		return count;
-	}
+            // Linear time
+            int leftIndex = 0;
+            int rightIndex = arr.length - 1;
+            while (leftIndex < rightIndex && arr[leftIndex] < 0 && arr[rightIndex] > 0) {
+                if (-arr[leftIndex] == arr[rightIndex]) {
+                    System.out.println("Numbers: " + arr[leftIndex]);
+                    count++;
+                    leftIndex++;
+                    rightIndex--;
+                } else if (-arr[leftIndex] > arr[rightIndex]) {
+                    leftIndex++;
+                } else if (-arr[leftIndex] < arr[rightIndex]) {
+                    rightIndex--;
+                }
+            }
 
-	@Override
-	public void methodToBePerformed(int[] arr) {
-		this.count(arr);
-	}
-}
-/** Growth rate is N^2*/
-class ThreeSumFaster extends DoublingTestTemplate {
-	public int count(int[] arr) {
-		int count = 0;
-		// Sort -> NlogN
-		Arrays.sort(arr);
+            return count;
+        }
 
-		// Linear time
-		for (int i = 0; i < arr.length; i++) {
-			int leftIndex = i + 1;
-			int rightIndex = arr.length - 1;
-			while (leftIndex < rightIndex && (arr[i] + arr[leftIndex]) < 0 && arr[rightIndex] > 0) {
-				if (-(arr[i] + arr[leftIndex]) == arr[rightIndex]) {
+        @Override
+        public void methodToBePerformed(int[] arr) {
+            this.count(arr);
+        }
+    }
+
+    /**
+     * Growth rate is N^2
+     */
+    private static class ThreeSumFaster extends DoublingTestTemplate {
+        public int count(int[] arr) {
+            int count = 0;
+            // Sort -> NlogN
+            Arrays.sort(arr);
+
+            // Linear time
+            for (int i = 0; i < arr.length; i++) {
+                int leftIndex = i + 1;
+                int rightIndex = arr.length - 1;
+                while (leftIndex < rightIndex && (arr[i] + arr[leftIndex]) < 0 && arr[rightIndex] > 0) {
+                    if (-(arr[i] + arr[leftIndex]) == arr[rightIndex]) {
 //					System.out.println("Numbers: " + arr[i] + " " + arr[negativeIndex] + " " + arr[positiveIndex]);
-					count++;
-					leftIndex++;
-					rightIndex--;
-				} else if (-(arr[i] + arr[leftIndex]) > arr[rightIndex]) {
-					leftIndex++;
-				} else if (-(arr[i] + arr[leftIndex]) < arr[rightIndex]) {
-					rightIndex--;
-				}
-			}
-		}
+                        count++;
+                        leftIndex++;
+                        rightIndex--;
+                    } else if (-(arr[i] + arr[leftIndex]) > arr[rightIndex]) {
+                        leftIndex++;
+                    } else if (-(arr[i] + arr[leftIndex]) < arr[rightIndex]) {
+                        rightIndex--;
+                    }
+                }
+            }
 
-		return count;
-	}
+            return count;
+        }
 
-	@Override
-	public void methodToBePerformed(int[] arr) {
-		this.count(arr);
-	}
+        @Override
+        public void methodToBePerformed(int[] arr) {
+            this.count(arr);
+        }
+    }
 }
--- algorithms/src/main/java/com/isa/section1/chapter4/exercises/Exercise_1_4_2.java
+++ algorithms/src/main/java/com/isa/section1/chapter4/exercises/Exercise_1_4_2.java
@@ -1,44 +1,44 @@
 package com.isa.section1.chapter4.exercises;
 
 public class Exercise_1_4_2 {
-	public static void main(String[] args) {
-		int[] numbers = { Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MIN_VALUE - 1, Integer.MIN_VALUE + 2, 2, 3, -5 };
-		ThreeSum.count(numbers);
-	}
-}
+    public static void main(String[] args) {
+        int[] numbers = {Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MIN_VALUE - 1, Integer.MIN_VALUE + 2, 2, 3, -5};
+        ThreeSum.count(numbers);
+    }
 
-class ThreeSum {
+    private static class ThreeSum {
 
-	public static int count(int[] numbers) {
-		int count = 0;
-		// Combination of 3s
-		for (int i = 0; i < numbers.length; i++) {
-			for (int k = i + 1; k < numbers.length; k++) {
-				/**
-				 *  Prevent Overflow
-				 *  Owerflow occurs when i + k > Integer.MAX_VALUE (or i + k < Integer.MIN_VALUE)
-				 */
-				if (numbers[k] < 0) {
-					if (numbers[i] < Integer.MIN_VALUE - numbers[k]) {
-						System.out.printf("Overflow prevented for: %d and %d%n", numbers[i], numbers[k]);
-						continue;
-					}
-				} else {
-					if (numbers[i] > Integer.MAX_VALUE - numbers[k]) {
-						System.out.printf("Overflow prevented for: %d and %d%n", numbers[i], numbers[k]);
-						continue;
-					}
-				}
+        public static int count(int[] numbers) {
+            int count = 0;
+            // Combination of 3s
+            for (int i = 0; i < numbers.length; i++) {
+                for (int k = i + 1; k < numbers.length; k++) {
+                    /**
+                     *  Prevent Overflow
+                     *  Owerflow occurs when i + k > Integer.MAX_VALUE (or i + k < Integer.MIN_VALUE)
+                     */
+                    if (numbers[k] < 0) {
+                        if (numbers[i] < Integer.MIN_VALUE - numbers[k]) {
+                            System.out.printf("Overflow prevented for: %d and %d%n", numbers[i], numbers[k]);
+                            continue;
+                        }
+                    } else {
+                        if (numbers[i] > Integer.MAX_VALUE - numbers[k]) {
+                            System.out.printf("Overflow prevented for: %d and %d%n", numbers[i], numbers[k]);
+                            continue;
+                        }
+                    }
 
-				for (int m = k + 1; m < numbers.length; m++) {
-					if (numbers[i] + numbers[k] + numbers[m] == 0) {
-						System.out.println("Numbers: " + numbers[i] + " " + numbers[k] + " " + numbers[m]);
-						count++;
-					}
-				}
-			}
-		}
+                    for (int m = k + 1; m < numbers.length; m++) {
+                        if (numbers[i] + numbers[k] + numbers[m] == 0) {
+                            System.out.println("Numbers: " + numbers[i] + " " + numbers[k] + " " + numbers[m]);
+                            count++;
+                        }
+                    }
+                }
+            }
 
-		return count;
-	}
+            return count;
+        }
+    }
 }
